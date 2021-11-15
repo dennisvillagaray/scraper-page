f7e1f18a301fb7a0e8fd05cb5ba5eace7f0b0393
dennisvillagaray <dennis.villagarayg@gmail.com>
2021-11-14 23:29:11 -0500

    second commit

--- index.js
+++ index.js
@@ -1,57 +1,79 @@
 // @ts-check
 const { chromium } = require('playwright')
 
+const price = 710
+
 const shops = [
   {
-    vendor: 'Amazon',
+    vendor: 'Plaza Vea',
     hasSchema: false,
-    url: 'https://www.amazon.es/dp/B08H93ZRLL/ref=cm_sw_r_cp_apa_glt_i_91H0Z62WVDRT6FMW033Z?tag=eol00-21',
+    url: 'https://www.plazavea.com.pe/monitor-teros-te-3173n-27--ips-curvo-144-hz-1920x1080-fhd-hdmi---displayport-freesync-100104831/p',
     checkStock: async ({ page }) => {
-      const addToCartButton = await page.$$('#add-to-cart-button')
-      return addToCartButton.length > 0
+      const containerHandle = await page.$('.ProductCard__price--online > .ProductCard__content__price > .ProductCard__price__integer')
+      const innerHTML = await page.evaluate(([body]) => body.innerHTML, [containerHandle])
+      return innerHTML
     }
   },
   {
-    vendor: 'Microsoft',
+    vendor: 'Login store',
     hasSchema: false,
-    url: 'https://www.xbox.com/es-es/configure/8WJ714N3RBTL',
+    url: 'https://www.loginstore.com/monitor-teros-te-3173n-27-ips-curvo-144-hz-1920x1080-fhd-hdmi-displayport-freesync',
     checkStock: async ({ page }) => {
-      const content = await page.textContent('[aria-label="Finalizar la compra del pack"]')
-      return content.includes('Sin existencias') === false
+      const containerHandle = await page.$('#product-price-34699')
+      const innerHTML = await page.evaluate(([body]) => body.innerHTML, [containerHandle])
+      const getPrice = innerHTML.replace(/[^0-9]/g, '').substr(0, 3)
+      return getPrice
     }
   },
   {
-    vendor: 'Fnac',
+    vendor: 'Linio',
     hasSchema: true,
-    url: 'https://www.fnac.es/Consola-Xbox-Series-X-1TB-Negro-Videoconsola-Consola/a7732201',
+    url: 'https://www.linio.com.pe/p/monitor-teros-gaming-te-3173n-27-ips-curvo-144-hz-hdmi-displayport-freesync-n2kv88',
     checkStock: async ({ page }) => {
-      const notAvailableIcon = await page.$$('.f-buyBox-availabilityStatus-unavailable')
-      return notAvailableIcon.length === 0
+      const containerHandle = await page.$('.product-price-lg > .lowest-price >.price-main-md')
+      const innerHTML = await page.evaluate(([body]) => body.innerHTML, [containerHandle])
+      const getPrice = innerHTML.replace(/[^0-9]/g, '').substr(0, 3)
+      console.log('price -> ', getPrice)
+      return getPrice
     }
   },
   {
-    vendor: 'Corte Ingles',
-    hasSchema: false,
-    url: 'https://www.elcorteingles.es/videojuegos/A37047078',
+    vendor: 'Ripley',
+    hasSchema: true,
+    url: 'https://simple.ripley.com.pe/teros-monitor-gamer-te-3173n-27-ips-curvo-144hz-freesync-fhd-1920x1080-pmp00001348361?color80_fijo=negro&s=o',
     checkStock: async ({ page }) => {
-      const content = await page.textContent('#js_add_to_cart_desktop')
-      return content.includes('Agotado temporalmente') === false
+      const containerHandle = await page.$('.product-internet-price > .product-price')
+      const innerHTML = await page.evaluate(([body]) => body.innerHTML, [containerHandle])
+      const getPrice = innerHTML.replace(/[^0-9]/g, '').substr(0, 3)
+      return getPrice
+    }
+  },
+  {
+    vendor: 'Real Plaza',
+    hasSchema: true,
+    url: 'https://www.realplaza.com/monitor-teros-te-3173n-27--ips-curvo-144-hz-1920x1080-fhd-hdmi---displayport-freesync-313944/p',
+    checkStock: async ({ page }) => {
+      const containerHandle = await page.$('.realplaza-product-custom-0-x-productSummaryPrice__Option__OfferPrice__Color')
+      const innerHTML = await page.evaluate(([body]) => body.innerHTML, [containerHandle])
+      const getPrice = innerHTML.replace(/[^0-9]/g, '').substr(0, 3)
+      return getPrice
+    }
+  },
+  {
+    vendor: 'Promart',
+    hasSchema: true,
+    url: 'https://www.promart.pe/monitor-teros-te-3176n-27--ips-75hz-1920x1080-full-hd-hdmi-vga--100046613/p',
+    checkStock: async ({ page }) => {
+      const containerHandle = await page.$$('.js-price-price > span')
+      const innerHTML = await page.evaluate(([body]) => body.innerHTML, [containerHandle[1]])
+      const getPrice = innerHTML.replace(/[^0-9]/g, '').substr(0, 3)
+      return getPrice
     }
   }
-  // disabled for now because it0s not working properly
-  // {
-  // vendor: 'MediaMarkt',
-  // hasSchema: true,
-  // url: 'https://www.mediamarkt.es/es/product/_consola-microsoft-xbox-series-x-1-tb-ssd-negro-1487615.html',
-  // checkStock: async ({ page }) => {
-  // const content = await page.textContent('StyledInfoTypo-sc-1jga2g7-0.jlGGjh')
-  // return content.includes('Este artículo no está disponible actualmente.') === false
-  // }
-  // }
 ]
 
   ; (async () => {
-    const browser = await chromium.launch({ headless: false })
+    const browser = await chromium.launch({})//headless: false })
 
 
     for (const shop of shops) {
@@ -60,7 +82,10 @@ const shops = [
       await page.goto(url)
 
       const hasStock = await checkStock({ page })
-      console.log(`${vendor}: ${hasStock ? 'HAS STOCK!!!!' : 'Out Of Stock'}`)
+
+      const validatePrice = hasStock < price
+
+      console.log(`${vendor}: ${validatePrice ? 'Buen precio!!! \n S/ ' + hasStock : 'Muy caro!!! \n S/ ' + hasStock}`)
       await page.screenshot({ path: `screenshots/${vendor}.png` })
       await page.close()
     }
